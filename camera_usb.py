b238b121bf5a386be7a466a39e8a5582c80f5f39
Matt Kohls <mattkohls13@gmail.com>
2018-02-09 15:43:38 -0500

    Fixing merge mess

--- camera_usb.py
+++ camera_usb.py
@@ -6,7 +6,6 @@ import numpy as np
 
 avg = np.repeat(0.0, 100)
 
-
 class Camera(object):
     thread = None  # background thread that reads frames from camera
     frame = None  # current frame is stored here by background thread
@@ -15,18 +14,7 @@ class Camera(object):
     filename = '';
     frame_width = 0
     frame_height = 0
-<<<<<<< HEAD
     fps = 30 # The target frames per second we want to get to
-=======
-
-    max_width = 320
-    max_height = 240
-
-    blank_frame = np.zeros((max_height, max_width, 3), np.uint8) # Creates a black image when there is nothing on the camera
-
-    fps = 20 # The target frames per second we want to get to
-    fps_time = 1 / fps# How long max we have to wait to get that fps
->>>>>>> a68eb887bfc9e500deef759cab31328ad3856b74
 
     def initialize(self):
         if Camera.thread is None:
@@ -60,9 +48,6 @@ class Camera(object):
             Camera.status = False
         Camera.filename = filename
         print(Camera.status)
-        #if(status):
-        #    fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        #    global video = cv2.VideoWriter('./pictures/video.avi', fourcc, 30, (int(cap.get(320), int(240)))
 
     @classmethod
     def _thread(cls):
@@ -99,45 +84,4 @@ class Camera(object):
                     started = False
                 break
 
-<<<<<<< HEAD
-=======
-            # To try to keep the framerate constant
-            waittime = cls.fps_time - (time.time() - timeon)
-            if waittime < 0:
-                waittime = 0
-            time.sleep(waittime)
-
-        cls.watcher = None
-
-
-    @classmethod
-    def _thread(cls):
-
-        #=========================
-        #    Video Settings      
-        #=========================
-
-        camera = cv2.VideoCapture(0)
-        cls.frame_width = int(camera.get(3)) # These pull the camera size from what opencv loads
-        cls.frame_height = int(camera.get(4))
-
-        while(True):
-            try:
-                ret, frame = camera.read()
-            except:
-                ret, frame = (-1, cls.blank_frame)
-
-            if ret == False:
-                frame = cls.blank_frame
-            
-            if cls.frame_width > cls.max_width or cls.frame_height > cls.max_height:
-                scaling = cls.max_width/float(cls.frame_width)
-                cls.frame = cv2.resize(frame, None, fx=scaling, fy=scaling, interpolation=cv2.INTER_AREA)
-            else:
-                cls.frame = frame
-            
-            if time.time() - cls.last_access > 2:
-                break
-
->>>>>>> a68eb887bfc9e500deef759cab31328ad3856b74
         cls.thread = None
